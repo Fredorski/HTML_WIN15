3fd0d1167075957f1db3006c1086322f0affe28d
Freddie Wahlgren <freddie.wahlgren@gmail.com>
2016-06-01 09:09:05 +0200

    First drawing in canvas

---
+++ F10_2016_06_01/scripts/drawCanvas.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function () {
+    //console.log("ready!");
+    ////alert('hej');
+    draw();
+});
+
+function draw() {
+    var canvas = document.getElementById('myform');
+    var ctx = canvas.getContext('2d');
+    ctx.fillRect(10, 20, 30, 200);
+    //samma kod med jQuery:
+    var jCanvas = $('myform');
+
+}
